9f0d884c3758cd082d74ab2fefcefbef54b0aa36
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-06-03 19:37:38 -0300

    agregamos funcionalidad 'listar'

--- productos/index.js
+++ productos/index.js
@@ -1,9 +1,9 @@
-//const {leerJSON,escribirJSON} = require('../data');
+const {leerJSON,escribirJSON} = require('../data');
 //const Producto = require('./Producto');
 
 module.exports = {
     //   productos : leerJSON(),
-    //   listar : require('./listar'),
+       listar : require('./listar'),
     //   agregar:  require('./agregar'),
     //   filtrar :  require('./filtar'),
     //   editar : require('./editar'),
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # libreria
-## jorgitoluis-lider
-## IsmaelAxel-Colaborador-[https://github.com/IsmaelAxel]
+## para usar este codigo tiene que usar "npm init -y" y "npm install colors" en la consola
+### jorgitoluis-lider
+### IsmaelAxel-Colaborador-[https://github.com/IsmaelAxel]
